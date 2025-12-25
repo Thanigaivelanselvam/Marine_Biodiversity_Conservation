5496c1cdb7feeb3cccf02eb91bfd1a7f30c0b517
Thanigaivelan Selvam <thanigaivelanselvam@gmail.com>
2025-12-25 14:13:57 +0530

    Modified the code and added some pages

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.skiez.marine_trust"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     compileOptions {
